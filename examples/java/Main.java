07e9e0fb527d2b933399575ba098c8d0e25e727c
Olof Bjarnason <olof.bjarnason@gmail.com>
2017-03-09 21:38:21 +0100

    Update Main.java

--- examples/java/Main.java
+++ examples/java/Main.java
@@ -6,42 +6,42 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
 public class Main {
-	public static void main( String[] args ) {
-		try {
-      // Lamp ID and color/period parameters
-			String lampId = "my-test-lamp";
-			String urlParameters = "color=#FFFFFF&period=500";
+    public static void main(String[] args) {
+        try {
+            // System ID and color/period parameters
+            String systemId = "my-test-system";
+            String urlParameters = "color=#FFFFFF&period=500"; // Color and period
+            //String urlParameters = "color=#00FF00"; // Only color
 
-      // Send POST request to CILamp API
-      String url = "https://api.cilamp.se/v1/"+ lampId +"/";
-			System.out.println("\nSending 'POST' request to URL : " + url);
-			System.out.println("Post parameters : " + urlParameters);
-			URL obj = new URL(url);
-			HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
-			connection.setRequestMethod("POST");
-			connection.setDoOutput(true);
-      connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-      connection.connect();
-			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-			wr.writeBytes(urlParameters);
-			wr.flush();
-			wr.close();
+            // Send POST request to CILamp API
+            String url = "https://api.cilamp.se/v1/" + systemId + "/";
+            System.out.println("Sending 'POST' request to URL: " + url);
+            System.out.println("Post parameters: " + urlParameters);
+            URL obj = new URL(url);
+            HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.connect();
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
 
-			int responseCode = connection.getResponseCode();
-			System.out.println("Response Code : " + responseCode);
+            // Read response code and string
+            int responseCode = connection.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in .readLine()) != null) {
+                response.append(inputLine);
+            } in .close();
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-
-			System.out.println(response.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}	
+            // Print result
+            System.out.println("Response code: " + responseCode);
+            System.out.println("Response string: " + response.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-	
