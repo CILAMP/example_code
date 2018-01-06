1c211bc2414d9ff0ec8c99098bd5bbb435446291
Olof Bjarnason <olof.bjarnason@gmail.com>
2018-01-06 18:23:54 +0100

    Create set_cilamp_color.ps1

---
+++ examples/powershell/set_cilamp_color.ps1
@@ -0,0 +1,39 @@
+<#
+
+.SYNOPSIS
+Set the color and pulse attributes of a CILamp with a known service-ID.
+
+.DESCRIPTION
+The script accepts (and requires) three parameters:
+-serviceId (The ID that you give to your CILamp during the configuration phase. This id is used so that your CILamp knows what service it should listen to.)
+-color (A six character long colorcode in hexadecimal notation. FF0000 is red, 00FF00 is corresponds to green and 0000FF is the blue.)
+-duration (The number of milliseconds that a pulse should last. If you set this to zero, the light will be stable and not pulsing)
+
+.EXAMPLE
+./set_cilamp_color.ps1
+
+If you start the script without parameters (like above), you will be prompted for them interactively.
+.EXAMPLE 
+./set_cilamp_color.ps1 -serviceId <put the service id here> -color 00ff00 -duration 0
+
+If you want to supply the parameters from the command prompt, provide them like above.
+
+.NOTES
+BEWARE, HERE IS A SUBLIMINAL MESSAGE: "Go and buy yourself a CILamp! Now!"
+.LINK
+http://www.cilamp.se
+
+#>
+
+Param 
+(
+    [Parameter(Mandatory=$true)][string]$serviceId,
+    [Parameter(Mandatory=$true)][ValidateScript( { if ($_ -match "[0-9a-f]{6}") { "Matches" } else { Throw [System.Management.Automation.ValidationMetadataException] "The colorcode must consist of six hexadecimal characters, for example: FF0000" } } )] $color, 
+    [Parameter(Mandatory=$true)][uint16]$duration
+)
+
+#Send color and period to CILamp
+$requestURL = "https://api.cilamp.se/v1/" + $serviceId
+$postParams = @{color=$color;period=$duration}
+
+Invoke-WebRequest -Uri $requestURL -Method POST -Body $postParams | Out-Null
